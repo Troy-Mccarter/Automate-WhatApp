6a7309a09ce56fe0ea8039097df343de17bbe637
repozitorii <89711616+repozitorii@users.noreply.github.com>
2021-08-31 00:57:07 +0530

    Update automate_whatsapp_message.py

--- automate_whatsapp_message.py
+++ automate_whatsapp_message.py
@@ -1,5 +1,5 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+1XXXXXXXXXX", "This message will be send", 4, 50)
-                        # here '+1' is country code, 'XXXXXXXXXX' is WhatsApp number,
-                        # '4' is hour in 24 hour format and '50' is minute
+pywhatkit.sendwhatmsg("+1XXXXXXXXXX", "This message will be send", 4, 50)  # here '+1' is country code, 'XXXXXXXXXX' is WhatsApp number, '4' is hour in 24 hour format and '50' is minute
+                        
+                        
